89b6f10b75a989d223b2814d2612b4a313f9638d
Andrii-Petrunia <petrunyandrey@gmail.com>
2025-03-02 20:47:01 +0100

    nodjs-hw-02

---
+++ src/server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      data: contacts,
+    });
+  });
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (contact === null) {
+      return res.status(404).json({
+        message: 'Contact not found',
+      });
+    }
+    res.status(200).json({
+      data: contact,
+    });
+  });
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
