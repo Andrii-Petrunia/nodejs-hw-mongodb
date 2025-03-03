412e4670ed1ce3d5404357cb4c6f0ddf4c5342b8
Andrii-Petrunia <petrunyandrey@gmail.com>
2025-03-03 09:08:37 +0100

    njs-hw-02

--- src/server.js
+++ src/server.js
@@ -22,6 +22,8 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
       data: contacts,
     });
   });
@@ -35,6 +37,8 @@ export const setupServer = () => {
       });
     }
     res.status(200).json({
+      status: 200,
+      message: 'Successfully found contact with id {contactId}!',
       data: contact,
     });
   });
